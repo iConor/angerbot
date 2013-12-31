a1e25bf29a90592d1bfdd45e281af22fb698c4e8
Conor Cary <conor@steve.gateway.pace.com>
2013-12-31 02:30:09 -0500

    Prepared Arduino for red/amber lights.

--- lego_pf_ir_arduino/lego_pf_ir_arduino.ino
+++ lego_pf_ir_arduino/lego_pf_ir_arduino.ino
@@ -1,30 +1,101 @@
 #include<legopowerfunctions.h>
 
-long BAUD_RATE = 57600;
-int IR_LED_PIN = 4;
+const long BAUD_RATE = 57600;
+const int PACKET_SIZE = 5;
+const int PACKET_START = '#';
 
-int syncByte = '#';
 int checkByte;
 
 int bluePWM, redPWM;
 
-LEGOPowerFunctions irSend( IR_LED_PIN );
+LEGOPowerFunctions irTransmit( IR_TRANSMIT_PIN );
 
-void setup()
-{ 
-  Serial.begin( BAUD_RATE ); 
+const int IR_TRANSMIT_PIN = 4;
+const int LEFT_BLINKER_PIN = 8;
+const int RIGHT_BLINKER_PIN = 9;
+const int BRAKE_LIGHT_PIN = 7;
+const int REVERSE_LIGHT_PIN = 10;
+
+enum AmberLights { 
+  OFF, LEFT, RIGHT, HAZARD };
+AmberLights indicators = HAZARD;
+AmberLights previousIndicators = HAZARD;
+
+boolean leftIndicatorOn = false;
+boolean rightIndicatorOn = false;
+
+const unsigned long DUTY_CYCLE = 375;
+unsigned long blinkerTimer = 0;
+
+int throttle = 1;
+int previousThrottle = 0;
+
+void setup() {
+  Serial.begin( BAUD_RATE );
+  pinMode( LEFT_BLINKER_PIN, OUTPUT );
+  pinMode( RIGHT_BLINKER_PIN, OUTPUT );
+  pinMode( BRAKE_LIGHT_PIN, OUTPUT );
 }
-void loop()
-{
-  if( Serial.available() >= 4 )
-  {
-    if( Serial.read() == syncByte )
-    {
+
+void loop() {
+  if( Serial.available() >= PACKET_SIZE ) {
+    if( Serial.read() == PACKET_START ) {
       checkByte = Serial.read();
+      indicators = static_cast<AmberLights>(Serial.read());
       bluePWM = Serial.read();
       redPWM = Serial.read();
-      irSend.ComboPWM( bluePWM, redPWM, CH1 );
-      Serial.write( checkByte );
+      irTransmit.ComboPWM( bluePWM, redPWM, CH1 );
+      Serial.write( indicators );
+      /*
+       * Brake Lights
+       */
+      throttle = redPWM;
+      if( throttle == 8 && throttle != previousThrottle ) {
+        digitalWrite( BRAKE_LIGHT_PIN, HIGH );
+      } 
+      else if ( previousThrottle == 8 && throttle != previousThrottle ) {
+        digitalWrite( BRAKE_LIGHT_PIN, LOW );
+      }
+      previousThrottle = throttle;
+      /*
+       * Amber Lights
+       */
+      if( indicators != previousIndicators ) {
+        leftIndicatorOn = indicatorFlipFlop( true, LEFT_BLINKER_PIN );
+        rightIndicatorOn = indicatorFlipFlop( true, RIGHT_BLINKER_PIN );
+        blinkerTimer = 0;
+      }
+      previousIndicators = indicators;
     }
   }
+  /*
+   * Blinkers and Flashers
+   */
+  if( indicators != OFF && millis() >= blinkerTimer ) {
+    if( indicators == LEFT ) {
+      leftIndicatorOn = indicatorFlipFlop( leftIndicatorOn, LEFT_BLINKER_PIN );
+    } 
+    else if( indicators == RIGHT ) {
+      rightIndicatorOn = indicatorFlipFlop( rightIndicatorOn, RIGHT_BLINKER_PIN );
+    }
+    else if( indicators == HAZARD ) {
+      leftIndicatorOn = indicatorFlipFlop( leftIndicatorOn, LEFT_BLINKER_PIN );
+      rightIndicatorOn = indicatorFlipFlop( rightIndicatorOn, RIGHT_BLINKER_PIN );
+    }
+    blinkerTimer = millis() + DUTY_CYCLE;
+  }
+}
+/*
+ * Swap indicator states:
+ */
+boolean indicatorFlipFlop( boolean indicatorOn, int indicatorPin ) {
+  if( indicatorOn ){
+    digitalWrite( indicatorPin, LOW );
+    indicatorOn = false;
+  }
+  else {
+    digitalWrite( indicatorPin, HIGH );
+    indicatorOn = true;
+  }
+  return indicatorOn;
 }
